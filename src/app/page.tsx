39db89b4dbabebd2ea9f343c222d019586c2f5e8
Amalia Putri <amaliaputriii2104@gmail.com>
2026-02-14 10:30:07 +0700

    feat: direct to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
-import Dashboard from "@/app/dashboard/page";
-import { Metadata } from "next";
+"use client";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="flex relative min-h-screen flex-col overflow-hidden items-center justify-center gap-20 bg-custom-blue z-0">
-      <Dashboard />
-    </main>
-  );
-}
+  const router = useRouter();
 
-export const metadata: Metadata = {
-  title: "Student Wellbeing & Outcome Optimization",
-  description: "Track your progress at a human pace",
-  generator: "Next.js",
-  keywords: ["Next.js", "React", "Productivity", "Student Wellbeing"],
-  applicationName: "Realrack",
-  category: "Productivity",
-};
+  useEffect(() => {
+    router.replace("/dashboard");
+  }, [router]);
+
+  return null;
+}
